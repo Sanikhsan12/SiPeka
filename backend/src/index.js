a61f1664b2b5f89255627932b3aa5090075c9676
Sanikhsan12 <lordkazumasan12@gmail.com>
2026-05-08 22:40:09 +0700

    im tired

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,16 +13,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(
-  cors({
-    origin: function (origin, callback) {
-      console.log("Request masuk dari origin:", origin);
-      // Kasih izin sementara (true) ke semua origin buat nge-test doang
-      callback(null, true);
-    },
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization", "Accept"],
-  }),
+  cors(),
 );
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
